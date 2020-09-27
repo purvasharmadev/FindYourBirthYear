5aeb86699a6a75a5953d810fa1c8c186ba540414
Purva <purva08815294379@gmail.com>
2020-09-27 12:42:02 +0530

    BirthYear

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year = 2020;
+    int byear;
+    printf("enter you birth year\n");
+    scanf("%d",&byear);
+    printf("your age is %d ",year - byear);
+
+    return 0;
+}
